18801b44d8607c6ccac725dae0c4ba746f01ca28
Jonathan Lee <nathanee@seas.upenn.edu>
2023-11-02 01:24:02 -0400

    ctgov shiny app
    This is a copied code from lecture 10-04 and 10-11. You should change the filepath for dataset accordingly. We can start from here for the midterm project.

---
+++ ctgov-query/app.R
@@ -0,0 +1,170 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(duckdb)
+library(dplyr)
+library(DBI)
+library(DT)
+library(ggplot2)
+library(ctrialsgov)
+
+
+# 0. Midterm scheduling 
+
+# 1. Clean up the table column names X
+# 2. Allow multiple brief title keywords X
+# 3. Create a histogram of the phase 
+# 4. Organize files.
+# 5. Fix the Phase plot
+# 6. Plot the concurrent studies (adding a feature/capability).
+
+# Steps to adding a feature:
+# 1. Specify the feature.
+#   - What does it do?
+#   - What will be shown?
+# 2. Specify the interface
+#   - Where should it go?
+#   - Does it enhance the user experience?
+# 3. Implement the feature without the UI
+# 4. Integrate the feature.
+
+source("ct-util.R")
+max_num_studies = 1000
+
+con = dbConnect(
+  duckdb(
+    file.path("..", "..", "duckdb", "ctgov.duckdb"), 
+    read_only = TRUE
+  )
+)
+dbListTables(con)
+studies = tbl(con, "studies")
+sponsors = tbl(con, "sponsors")
+
+
+ctgov_load_duckdb_file(file.path("..", "..", "duckdb", "ctgov-derived.duckdb")) #derived data
+
+endpoints = ctgov_query_endpoint()
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Clinical Trials Query"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      #          sliderInput("bins",
+      #                      "Number of bins:",
+      #                      min = 1,
+      #                      max = 50,
+      #                      value = 30),
+      textInput("brief_title_kw", "Brief title keywords"),
+      checkboxGroupInput("source_class", 
+                         label = h3("Sponsor Type"), 
+                         choices = list("Federal" = "FED", 
+                                        "Individual" = "INDIV", 
+                                        "Industry" = "INDUSTRY",
+                                        "Network" = "NETWORK",
+                                        "NIH" = "NIH",
+                                        "Other" = "OTHER",
+                                        "Other gov" = "OTHER_GOV",
+                                        "Unknown" = "UNKNOWN")),
+      ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Plot", plotOutput("distPlot")),
+        tabPanel("Endpoint Met", plotOutput("endpointPlot")),
+        tabPanel("Phase", plotOutput("phase_plot")),
+        tabPanel("Concurrent", plotOutput("concurrent_plot"))
+      ),
+      dataTableOutput("trial_table")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # browser() #for debugging
+    create_phase_histogram_plot(studies, input$brief_title_kw)
+  })
+  
+  output$endpointPlot <- renderPlot({
+    create_endpoint_histogram(studies, endpoints, input$brief_title_kw)
+  })
+  
+  
+  output$trial_table = renderDataTable({
+    si = trimws(unlist(strsplit(input$brief_title_kw, ",")))
+    title_kw_search(studies, si) |>
+      select(nct_id, brief_title, start_date, completion_date) |>
+      rename(`NCT ID` = nct_id, `Brief Title` = brief_title,
+             `Start Date` = start_date, `Completion Date` = completion_date) |>
+      head(1000)
+  })
+  
+  # 10/11/2023
+  get_studies = reactive({
+    if (input$brief_title_kw != "") {
+      si = input$brief_title_kw |>
+        strsplit(",") |>
+        unlist() |>
+        trimws()
+      ret = query_kwds(studies, si, "brief_title", match_all = TRUE)
+    } else {
+      ret = studies
+    }
+    # browser()
+    if (!is.null(input$source_class)) {
+      ret = ret |> 
+        filter(source_class %in% !!input$source_class)
+        # filter(source_class, si, "brief_title", match_all = TRUE)
+    }
+    # input$source_class
+    
+    ret |> 
+      head(max_num_studies) |>
+      collect()
+  })
+  
+  output$phase_plot = renderPlot({
+    get_studies() |>
+      plot_phase_histogram()
+  })
+  
+  output$concurrent_plot = renderPlot({
+    get_studies() |>
+      select(start_date, completion_date) |>
+      get_concurrent_trials() |>
+      ggplot(aes(x = date, y = count)) +
+      geom_line() +
+      xlab("Date") +
+      ylab("Count") + 
+      theme_bw()
+  })
+  
+  output$trial_table = renderDataTable({
+    get_studies() |> 
+      head(max_num_studies) |>
+      select(nct_id, brief_title, start_date, completion_date) |>
+      rename(`NCT ID` = nct_id, `Brief Title` = brief_title,
+             `Start Date` = start_date, `Completion Date` = completion_date)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ ctgov-query/ct-util.R
@@ -0,0 +1,152 @@
+query_kwds <- function(tbl, kwds, column, ignore_case = TRUE, match_all = FALSE) {
+  kwds <- paste0("%", kwds, "%") |>
+    gsub("'", "''", x = _)
+  if (ignore_case) {
+    like <- " ilike "
+  } else{
+    like <- " like "
+  }
+  query <- paste(
+    paste0(column, like, "'", kwds, "'"),
+    collapse = ifelse(match_all, " AND ", " OR ")
+  )
+  
+  dplyr::filter(tbl, dplyr::sql(query))
+}
+
+title_kw_search = function(studies, kw) {
+  query_kwds(studies, kw, "brief_title", match_all = TRUE) |>
+    collect()
+}
+
+cumulative_studies = function(s) {
+  
+}
+
+create_phase_histogram_plot = function(studies, brief_title_kw) { #added
+  d = title_kw_search(studies, brief_title_kw) |>
+    head(1000)
+  d$phase[is.na(d$phase)] = "NA"
+  d = d |>
+    select(phase) |>
+    group_by(phase) |>
+    summarize(n = n())
+  ggplot(d, aes(x = phase, y = n)) +
+    geom_col() +
+    theme_bw() +
+    xlab("Phase") +
+    ylab("Count")
+}
+
+create_endpoint_histogram = function(studies, endpoints, kw) {
+  em = query_kwds(studies, kw, "brief_title", match_all = TRUE) |>
+    select(nct_id) |>
+    collect() |>
+    left_join(endpoints, by = "nct_id") |>
+    group_by(endpoint_met) |>
+    summarize(n = n())
+  
+  ggplot(em, aes(x = endpoint_met, y = n)) +
+    geom_col() +
+    scale_y_log10() +
+    theme_bw()
+}
+
+
+
+
+
+
+
+
+# 10/14/2023
+
+
+con = dbConnect(
+  duckdb(
+    file.path("..", "..", "duckdb", "ctgov.duckdb"), 
+    read_only = TRUE
+  )
+)
+
+if (length(dbListTables(con)) == 0) {
+  stop("Problem reading from connection.")
+}
+studies = tbl(con, "studies")
+sponsors = tbl(con, "sponsors")
+
+#' @title Query keywords from a database table.
+#' @description Description goes here.
+#' @param d the database table.
+#' @param kwds the keywords to look for.
+#' @param column the column to look for the keywords in.
+#' @param ignore_case should the case be ignored when searching for a keyword?
+#' (default TRUE)
+#' @param match_all should we look for values that match all of the keywords 
+#' (intersection) or any of the keywords (union)? (default FALSE; union).
+query_kwds <- function(d, kwds, column, ignore_case = TRUE, match_all = FALSE) {
+  kwds = kwds[kwds != ""]
+  kwds = paste0("%", kwds, "%") |>
+    gsub("'", "''", x = _)
+  if (ignore_case) {
+    like <- " ilike "
+  } else{
+    like <- " like "
+  }
+  query = paste(
+    paste0(column, like, "'", kwds, "'"),
+    collapse = ifelse(match_all, " AND ", " OR ")
+  )
+  filter(d, sql(query)) 
+}
+
+# Create a histogram of the phases returned by a brief title keyword search
+# @param d the database table.
+# @param brief_title_kw the brief title keywords to look for. This is optional.
+plot_phase_histogram = function(x) {
+  x$phase[is.na(x$phase)] = "NA"
+  x = x |>
+    select(phase) |>
+    group_by(phase) |>
+    summarize(n = n()) 
+  
+  ggplot(x, aes(x = phase, y = n)) +
+    geom_col() +
+    theme_bw() +
+    xlab("Phase") +
+    ylab("Count")
+}
+
+#' Get the number of concurrent trials for each date in a set of studies
+#' @param d the studies to get the number of concurrent trials for.
+#' @return A tibble with a `date` column and a `count` of the number of
+#' concurrent trials at that date.
+get_concurrent_trials = function(d) {
+  # browser()
+  # Get all of the unique dates.
+  all_dates = d |> 
+    pivot_longer(cols = everything()) |>
+    select(-name) |>
+    distinct() |> 
+    arrange(value) |>
+    na.omit() |> 
+    rename(date = value)
+  
+  within_date = function(date, starts, ends) {
+    date >= starts & date <= ends
+  }
+  
+  # Get the number of concurrent trials at each of the unique dates.
+  all_dates$count = 
+    map_dbl(
+      all_dates$date, 
+      ~ .x |> 
+        within_date(d$start_date, d$completion_date) |>
+        sum(na.rm = TRUE)
+    )
+  return(all_dates)
+}
+
+plot_concurrent_studies = function(studies) {
+  plot(mtcars$mpg, mtcars$cyl)
+}
